e63bd9c34c0c281247006f1010989359f55c3c32
michaelbaker-boop <michaelbaker4@my.whccd.edu>
2025-01-21 01:02:04 -0800

    Step 3.1

--- COMP003A.LectureActivity2/Program.cs
+++ COMP003A.LectureActivity2/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace COMP003A.LectureActivity2
+﻿// Author: Michael Baker
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: A program demonstrating statements, variables, and operators.
+namespace COMP003A.LectureActivity2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userName;
+            int userAge;
+            double productPrice;
+            bool isStudent;
+
+            Console.Write("Enter your name: ");
+            userName = Console.ReadLine(); // Read and assign user's input to username
+
+            Console.Write("Enter your age: ");
+            userAge = int.Parse(Console.ReadLine()); // Convert and assign input to userAge
+
+            Console.Write("Enter the price of a product: ");
+            productPrice = double.Parse(Console.ReadLine()); // Convert and assign input to productPrice
+
+            Console.Write("Are you a student? (true/false): ");
+            isStudent = bool.Parse(Console.ReadLine()); // Convert and assign input to isStudent
+
         }
     }
 }
